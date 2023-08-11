167356739560672743d06a8cdf53af9e12874ef5
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-08-10 23:18:07 -0300

    feat: create AppContextProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AppContextProvider } from '@/contexts';
 import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <AppContextProvider>
+                <body className={inter.className}>{children}</body>
+            </AppContextProvider>
         </html>
     );
 }
---
+++ src/contexts/index.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+
+type ComposedContextsProps = {
+    components: Array<
+        React.JSXElementConstructor<React.PropsWithChildren<unknown>>
+    >;
+    children: React.ReactNode;
+};
+
+function ComposedContexts(props: ComposedContextsProps) {
+    const { components, children } = props;
+
+    return (
+        <>
+            {components.reduceRight((acc, CurrContext) => {
+                return <CurrContext>{acc}</CurrContext>;
+            }, children)}
+        </>
+    );
+}
+
+export type AppContextProviderProps = { children: ReactNode };
+
+export function AppContextProvider({ children }: AppContextProviderProps) {
+    return <ComposedContexts components={[]}>{children}</ComposedContexts>;
+}
