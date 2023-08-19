f9ec9829e0451a8509e99c670e37a452b3c85971
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-08-18 22:39:44 -0300

    refactor: remove useCase folder

--- src/useCases/auth.ts
+++
@@ -1,8 +0,0 @@
-import { getRequestCookie } from '@/storage/cookies/server';
-import { NextRequest } from 'next/server';
-
-export const sessionIsActive = (req: NextRequest) => {
-    const token = getRequestCookie('s-p-guard-admin:token', req);
-
-    return !!token;
-};
