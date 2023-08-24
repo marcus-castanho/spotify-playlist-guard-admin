17b2906a6ce1cac05b57d13493d56d973364a642
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-08-23 23:37:52 -0300

    feat: create base structure for EditExternalApp form

--- src/components/EditExternalAppModal/index.tsx
+++ src/components/EditExternalAppModal/index.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useExternalApp } from './hooks/useExternalApp';
+import { ExternalApp } from '@/services/spotifyPlaylistGuardApi';
 
 export type EditExternalAppModalProps = {
     externalAppId: string;
@@ -14,20 +15,78 @@ export const EditExternalAppModal: FC<EditExternalAppModalProps> = ({
     return (
         <>
             {externalApp && (
-                <div>
-                    {'{'}
-                    {Object.keys(externalApp).map((key) => {
-                        return (
-                            <div key={key}>
-                                {`${key}: ${
-                                    externalApp[key as keyof typeof externalApp]
-                                }`}
-                            </div>
-                        );
-                    })}
-                    {'}'}
-                </div>
+                <>
+                    <div style={{ border: '1px solid gray' }}>
+                        <div>{`id: ${externalApp.id}`}</div>
+                        <div>{`createdAt: ${externalApp.createdAt}`}</div>
+                        <div>{`updatedAt: ${externalApp.updatedAt}`}</div>
+                    </div>
+                    <ExternalAppForm
+                        defaultForm={{
+                            name: externalApp.name,
+                            baseUrl: externalApp.baseUrl,
+                            recoverEmail: externalApp.recoverEmail,
+                        }}
+                    />
+                </>
             )}
         </>
     );
 };
+
+type ExternalAppFormProps = {
+    defaultForm: Pick<ExternalApp, 'name' | 'recoverEmail' | 'baseUrl'>;
+};
+
+function ExternalAppForm({ defaultForm }: ExternalAppFormProps) {
+    const [form, setForm] = useState(defaultForm);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label style={{ display: 'flex', justifyContent: 'space-between' }}>
+                Name
+                <input
+                    type="text"
+                    value={form.name}
+                    onChange={({ target }) =>
+                        setForm((state) => ({
+                            ...state,
+                            name: target.value,
+                        }))
+                    }
+                />
+            </label>
+            <label style={{ display: 'flex', justifyContent: 'space-between' }}>
+                Recover email
+                <input
+                    type="text"
+                    value={form.recoverEmail}
+                    onChange={({ target }) =>
+                        setForm((state) => ({
+                            ...state,
+                            recoverEmail: target.value,
+                        }))
+                    }
+                />
+            </label>
+            <label style={{ display: 'flex', justifyContent: 'space-between' }}>
+                Base URL
+                <input
+                    type="text"
+                    value={form.baseUrl}
+                    onChange={({ target }) =>
+                        setForm((state) => ({
+                            ...state,
+                            baseUrl: target.value,
+                        }))
+                    }
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
