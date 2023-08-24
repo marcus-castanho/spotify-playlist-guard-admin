b231dbe803dbb0fba4c7271d9cab3a266bbbdb8e
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-08-23 22:47:35 -0300

    feat: create useExternalApp hook

---
+++ src/components/ExternalAppsList/hooks/useExternalApp.ts
@@ -0,0 +1,26 @@
+import { QueryKey } from '@/contexts/QueryContext';
+import { useClientErrorHandler } from '@/errors/clientErrorHandlers';
+import { getExternalApp } from '@/services/spotifyPlaylistGuardApi';
+import { getCookie } from '@/storage/cookies/client';
+import { useQuery } from '@tanstack/react-query';
+
+export function useExternalApp(id: string | null) {
+    const token = getCookie('s-p-guard-admin:token') || '';
+    const { handleGuardApiResponse } = useClientErrorHandler();
+    const externalAppsQueryKey: QueryKey = 'external-apps';
+
+    const externalAppsQuery = useQuery([externalAppsQueryKey, id], {
+        queryFn: () => {
+            if (!id) return null;
+            return getExternalApp({ id, authToken: token })
+                .then(handleGuardApiResponse)
+                .catch(() => null);
+        },
+        initialData: null,
+        enabled: !!id,
+    });
+
+    return {
+        externalApp: externalAppsQuery.data,
+    };
+}
