a99c44e72db5fb3f189de55c3f2de090e30c3839
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-08-31 21:31:08 -0300

    feat: run middleware on every route and verify isPrivate inside of it

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,23 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { handleMiddlewareErrorResponse } from './errors/serverErrorHandlers';
 import { validateSession } from './middlewares/validateSession';
+import { isPrivatePage } from './config/pages';
 
-/**
- * middleware runs only on private routes
- */
 export const config = {
-    matcher: ['/home/:path*', '/profile/:path*'],
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
 
 export async function middleware(request: NextRequest) {
     try {
-        validateSession(request);
+        if (isPrivatePage(request.nextUrl.pathname)) validateSession(request);
+
         return NextResponse.next();
     } catch (error) {
         const response = NextResponse.next();
