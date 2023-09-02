d869f0549c21e54d91d65cc18ef495ffac6e85f0
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-09-01 21:56:42 -0300

    feat: use matcher config to on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,17 @@ import { validateSession } from './middlewares/validateSession';
 import { isPrivatePage } from './config/pages';
 import { shouldRunMiddlewares } from './middlewares/shouldRunMiddlewares';
 
+export const config = {
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
+
 export async function middleware(request: NextRequest) {
     try {
         if (!shouldRunMiddlewares(request)) return NextResponse.next();
