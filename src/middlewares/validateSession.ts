32c4ce4f6556a243641729c60e9589037f2fabac
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-09-01 21:55:05 -0300

    fix: import write Unauthorized error spell

--- src/middlewares/validateSession.ts
+++ src/middlewares/validateSession.ts
@@ -1,9 +1,9 @@
-import { UnauthorizedError } from '@/errors';
+import { Unauthorized } from '@/errors';
 import { getRequestCookie } from '@/storage/cookies/server';
 import { NextRequest } from 'next/server';
 
 export const validateSession = (req: NextRequest) => {
     const token = getRequestCookie('s-p-guard-admin:token', req);
 
-    if (!token) throw new UnauthorizedError({});
+    if (!token) throw new Unauthorized({});
 };
