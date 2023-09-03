1690607db5aba5381301ed6262985e7039c367e3
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-09-03 01:05:35 -0300

    refactor: change isPrivatePage verifier fn

--- src/config/pages.ts
+++ src/config/pages.ts
@@ -1,7 +1,19 @@
 export const publicPages = ['/', '/signin', '/500'] as const;
 export type PublicPage = (typeof publicPages)[number];
 
+export const privatePages = ['/external-apps', '/home', '/profile'] as const;
+export type PrivatePage = (typeof privatePages)[number];
+
+export const isPublicPage = (path: string) => {
+    return publicPages.some((page) => {
+        if (page === '/') return path === page;
+        console.log({ path, page });
+        return path.startsWith(page);
+    });
+};
+
 export const isPrivatePage = (path: string) => {
-    //@ts-ignore - includes show error since path is string and publicPages is tuple but the verification must be done nevertheless
-    return !publicPages.includes(path);
+    return privatePages.some((page) => {
+        return path.startsWith(page);
+    });
 };
