e7588a8d35861e095330b298c124e79eac8f2fe5
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-09-08 19:44:30 -0300

    feat: create /styles/colors specifications for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+const { colors } = require('./src/styles/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {
         extend: {},
+        colors,
     },
     plugins: [],
 };
