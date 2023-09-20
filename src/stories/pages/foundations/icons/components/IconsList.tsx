abef8dc10dcb9b037d7b91bfd29d41392328acc0
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-09-20 00:07:47 -0300

    feat: add tooltip to icon visualization

--- src/stories/pages/foundations/icons/components/IconsList.tsx
+++ src/stories/pages/foundations/icons/components/IconsList.tsx
@@ -1,4 +1,6 @@
 import React, { FC, ReactNode } from 'react';
+import { Tooltip } from '../../../../../components/Tooltip';
+import { linkTo } from '@storybook/addon-links';
 
 type IconsListProps = {
     icons: { title: string; component: ReactNode }[];
@@ -8,9 +10,22 @@ export const IconsList: FC<IconsListProps> = ({ icons }) => {
     return (
         <div style={{ paddingTop: '24px' }}>
             {icons.map((icon) => (
-                <div key={icon.title} style={{ display: 'inline-block' }}>
-                    {icon.component}
-                </div>
+                <Tooltip
+                    key={icon.title}
+                    content={icon.title}
+                    placement="bottom"
+                >
+                    <div
+                        className="hover:cursor-pointer"
+                        style={{
+                            display: 'inline-block',
+                            padding: '2px',
+                        }}
+                        onClick={linkTo('Foundations/Colors')}
+                    >
+                        {icon.component}
+                    </div>
+                </Tooltip>
             ))}
         </div>
     );
