ba314bb495fb0e01a21760ae880d77ba305ab7d8
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-09-30 22:48:08 -0300

    feat: add theme prop to decorator for components with theme variations

--- src/stories/pages/components/decorators.tsx
+++ src/stories/pages/components/decorators.tsx
@@ -3,6 +3,7 @@ import { Decorator } from '@storybook/react';
 
 export const withColorScheme: Decorator = (Story, context) => {
     const { theme } = context.globals;
+    const { args } = context;
 
     if (theme === 'dark') {
         document.documentElement.classList.add('dark');
@@ -10,5 +11,6 @@ export const withColorScheme: Decorator = (Story, context) => {
         document.documentElement.classList.remove('dark');
     }
 
-    return <Story />;
+    /**Components in the application that have variations for themes accept a 'theme' prop */
+    return <Story args={{ ...args, theme }} />;
 };
