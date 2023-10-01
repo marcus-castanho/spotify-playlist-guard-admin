2c405fe31e8575bf9b30992564145f16d2b08e5b
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-01 01:27:03 -0300

    feat: create InfoIcon and doc

--- src/stories/pages/foundations/icons/icons.tsx
+++ src/stories/pages/foundations/icons/icons.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import { WarningIcon } from '@/components/icons/WarningIcon';
 import { WarningFilledIcon } from '@/components/icons/WarningFilledIcon';
 import { CheckIcon } from '@/components/icons/CheckIcon';
+import { InfoIcon } from '@/components/icons/InfoIcon';
 
 export const icons: { title: string; component: ReactNode }[] = [
     {
@@ -15,5 +16,9 @@ export const icons: { title: string; component: ReactNode }[] = [
     {
         title: 'check',
         component: <CheckIcon size={24} key={CheckIcon.name} />,
+    },
+    {
+        title: 'info',
+        component: <InfoIcon size={24} key={InfoIcon.name} />,
     },
 ];
