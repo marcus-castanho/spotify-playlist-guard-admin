985866d41d16886f0bfc064c2733a098e878191b
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-02 23:22:06 -0300

    feat: add ThemeSwitcher to index page

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
+import { ThemeSwitcherWithHook as ThemeSwitcher } from '@/components/ThemeSwitcher';
 
 export const Index = () => {
     return (
         <>
             <Link href="/signin">Sign in</Link>
+            <ThemeSwitcher />
         </>
     );
 };
