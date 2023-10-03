125fa837b0f6a06f7b01f1d293668b19424b3324
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-02 22:49:29 -0300

    feat: use SunIcon and MoonIcon in ThemeSwitcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,15 +1,29 @@
 'use client';
 
-import { useTheme } from '@/contexts/ThemeContext';
-import React from 'react';
+import { Theme } from '@/contexts/ThemeContext';
+import React, { FC } from 'react';
+import { MoonIcon } from './icons/MoonIcon';
+import { SunIcon } from './icons/SunIcon';
 
-export const ThemeSwitcher = () => {
-    const { theme, switchTheme } = useTheme();
+type ThemeSwitcherProps = {
+    theme: Theme;
+    switchTheme: (alternativeTheme: Theme) => void;
+};
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({
+    theme,
+    switchTheme,
+}) => {
     const alternativeTheme = theme === 'dark' ? 'light' : 'dark';
+    const alternativeThemeIcon =
+        theme === 'dark' ? (
+            <SunIcon size={24} fillColor="white" />
+        ) : (
+            <MoonIcon size={24} />
+        );
 
     return (
         <button onClick={() => switchTheme(alternativeTheme)}>
-            {alternativeTheme}
+            {alternativeThemeIcon}
         </button>
     );
 };
