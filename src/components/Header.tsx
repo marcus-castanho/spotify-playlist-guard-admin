db0e0c0bcb719560f23d2922c2ae1ad8e3f3c2ab
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-07 01:06:10 -0300

    feat: create Header component with ThemeSwitcher

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ThemeSwitcherWithHook as ThemeSwitcher } from '@/components/ThemeSwitcher';
+
+export const Header = () => {
+    return (
+        <>
+            <ThemeSwitcher />
+        </>
+    );
+};
