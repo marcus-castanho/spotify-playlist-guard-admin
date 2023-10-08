d3623ea6bf23e8c5744ea1da329ea93c932a14a6
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-08 01:15:17 -0300

    feat: use new inputs and button components in SignInForm

--- src/views/SignIn/components/SignInForm.tsx
+++ src/views/SignIn/components/SignInForm.tsx
@@ -5,14 +5,22 @@ import { postAuth } from '@/services/spotifyPlaylistGuardApi';
 import { useRouter } from 'next/navigation';
 import { useToast } from '@/contexts/ToastContext';
 import { handleUncaughtClientError } from '@/errors/clientErrorHandlers';
+import { TextInputField } from '@/components/TextInputField';
+import { PasswordInputField } from '@/components/PasswordInputField';
+import { FormRow } from '@/components/FormRow';
+import { ButtonPrimary } from '@/components/ButtonPrimary';
+import { useTheme } from '@/contexts/ThemeContext';
 
 export const SignInForm = () => {
     const [form, setForm] = useState({ email: '', password: '' });
     const router = useRouter();
     const { toast } = useToast();
+    const [isSubmiting, setIsSubmitting] = useState(false);
+    const { theme } = useTheme();
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        setIsSubmitting(true);
         postAuth(form)
             .then(({ success }) => {
                 if (!success) throw new Error('Failed to authenticate');
@@ -22,41 +30,46 @@ export const SignInForm = () => {
             .catch((error) => {
                 handleUncaughtClientError(error);
                 toast('Error while performing this operation', 'error');
-            });
+            })
+            .finally(() => setIsSubmitting(false));
     };
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Email
-                    <input
+            <form
+                onSubmit={handleSubmit}
+                className="top-1/2 max-w-xs rounded-lg bg-white p-3.5 dark:bg-black"
+            >
+                <FormRow columns={1}>
+                    <TextInputField
                         id="email"
-                        type="email"
-                        value={form.email}
-                        onChange={(event) =>
-                            setForm((state) => ({
-                                ...state,
-                                email: event.target.value,
-                            }))
+                        label="Email"
+                        defaultValue={''}
+                        required
+                        onChange={(value) =>
+                            setForm((state) => ({ ...state, email: value }))
                         }
                     />
-                </label>
-                <label>
-                    Password
-                    <input
+                </FormRow>
+                <FormRow columns={1}>
+                    <PasswordInputField
                         id="password"
-                        type="password"
-                        value={form.password}
-                        onChange={(event) =>
-                            setForm((state) => ({
-                                ...state,
-                                password: event.target.value,
-                            }))
+                        label="Password"
+                        required
+                        onChange={(value) =>
+                            setForm((state) => ({ ...state, password: value }))
                         }
+                        theme={theme}
                     />
-                </label>
-                <button type="submit">Sign In</button>
+                </FormRow>
+                <div className="p-4">
+                    <ButtonPrimary
+                        content="Sign In"
+                        type="submit"
+                        disabled={isSubmiting}
+                        stretch
+                    />
+                </div>
             </form>
         </>
     );
