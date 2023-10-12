062530fe659ae2a716af3ccd0d9425e7878b98c7
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-12 00:08:24 -0300

    feat: use ThemeProvider in the main stories decorator
    -remove old manual definition of theme and use theme context

--- src/stories/pages/components/decorators.tsx
+++ src/stories/pages/components/decorators.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Decorator } from '@storybook/react';
+import { AppContextProvider } from '@/contexts';
+import { CookiesProvider } from '@/contexts/CookiesContext';
+import { ThemeProvider, withTheme } from '@/contexts/ThemeContext';
 
 export const withColorScheme: Decorator = (Story, context) => {
     const { backgrounds } = context.globals;
-    const { args } = context;
     const theme = backgrounds?.value === 'black' ? 'dark' : 'light';
 
-    if (theme === 'dark') {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark');
-    }
-
-    /**Components in the application that have variations for themes accept a 'theme' prop */
-    return <Story args={{ ...args, theme }} />;
+    return (
+        <AppContextProvider
+            initialTheme={theme}
+            providers={[
+                CookiesProvider,
+                withTheme(ThemeProvider, { initialTheme: theme }),
+            ]}
+        >
+            <Story />
+        </AppContextProvider>
+    );
 };
