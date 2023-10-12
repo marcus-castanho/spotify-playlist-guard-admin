9252f6cb3e5bc972f15460420f3b69ddded2ede0
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-12 00:30:45 -0300

    feat: Reorder components - use Header

--- src/views/SignIn/index.tsx
+++ src/views/SignIn/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import Link from 'next/link';
 import { SignInForm } from './components/SignInForm';
 import { Header } from '@/components/Header';
 
 export const SignIn = () => {
     return (
-        <div className="flex h-screen flex-col from-gray-900 to-black dark:bg-gradient-to-b">
-            <div className="flex">
-                <Header />
-                <h1>Sign In</h1>
-                <Link href="/">Index</Link>
+        <>
+            <Header />
+            <div className="flex flex-1 flex-col from-gray-900 to-black dark:bg-gradient-to-b">
+                <div className="flex"></div>
+                <br />
+                <div className="flex w-full flex-1 items-center justify-center">
+                    <SignInForm />
+                </div>
             </div>
-            <br />
-            <div className="flex w-full flex-1 items-center justify-center">
-                <SignInForm />
-            </div>
-        </div>
+        </>
     );
 };
