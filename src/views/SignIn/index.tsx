bb9b881b04e8cc5707f7d6c7955e257e28f4df78
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-14 14:27:22 -0300

    feat: define positions of components in SignIn page

--- src/views/SignIn/index.tsx
+++ src/views/SignIn/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { SignInForm } from './components/SignInForm';
 import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 export const SignIn = () => {
     return (
-        <>
+        <div className="flex min-h-screen flex-col">
             <Header />
-            <div className="flex flex-1 flex-col from-gray-900 to-black dark:bg-gradient-to-b">
-                <div className="flex"></div>
-                <br />
-                <div className="flex w-full flex-1 items-center justify-center">
-                    <SignInForm />
-                </div>
-            </div>
-        </>
+            <main className="flex flex-1 items-center justify-center">
+                <SignInForm />
+            </main>
+            <Footer />
+        </div>
     );
 };
