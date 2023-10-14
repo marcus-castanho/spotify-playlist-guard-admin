541bd92432e7a30465c3b961a69fefe03e4748b1
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-14 19:37:30 -0300

    feat: use custom page comoponents in Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import Link from 'next/link';
 import { SignOutButton } from '@/components/SignOutButton';
 import { Header } from '@/components/Header';
+import { PageContainer } from '@/components/PageContainer';
+import { Footer } from '@/components/Footer';
+import { Main } from '@/components/Main';
 
 export const Home = () => {
     return (
-        <>
+        <PageContainer>
             <Header />
-            <h1>Home</h1>
-            <SignOutButton />
-            <br />
-            <Link href="/profile">Profile</Link>
-            <br />
-            <Link href="/external-apps">External Apps</Link>
-        </>
+            <Main>
+                <div className="flex gap-4">
+                    <SignOutButton />
+                    <Link href="/profile">Profile</Link>
+                    <Link href="/external-apps">External Apps</Link>
+                </div>
+            </Main>
+            <Footer />
+        </PageContainer>
     );
 };
