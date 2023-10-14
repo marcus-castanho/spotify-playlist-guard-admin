56027770cbe09396f46c86ea9fd7d89e86b7b9d0
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-14 18:30:33 -0300

    feat: add Sign in title in SignIn page

--- src/views/SignIn/index.tsx
+++ src/views/SignIn/index.tsx
@@ -8,7 +8,10 @@ export const SignIn = () => {
         <div className="flex min-h-screen flex-col">
             <Header />
             <main className="flex w-full flex-1 items-center justify-center">
-                <SignInForm />
+                <div>
+                    <h1 className="px-4 text-4xl md:py-4">Sign in</h1>
+                    <SignInForm />
+                </div>
             </main>
             <Footer />
         </div>
