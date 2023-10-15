bcb9ca827af147c18f425a14c11770428146ee8b
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-15 02:09:47 -0300

    fix: add user to Header story args

--- src/stories/pages/components/Header.stories.ts
+++ src/stories/pages/components/Header.stories.ts
@@ -11,5 +11,18 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {},
+    args: { user: null },
+};
+
+export const Authenticated: Story = {
+    args: {
+        user: {
+            id: '00000000-0000-0000-0000-000000000000',
+            name: 'Test user',
+            email: 'test@test.com',
+            roles: ['admin'],
+            createdAt: '2023-08-13T02:31:45.610Z',
+            updatedAt: '2023-10-11T01:18:06.082Z',
+        },
+    },
 };
