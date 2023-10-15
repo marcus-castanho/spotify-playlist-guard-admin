05f372c01d6056b20e249f052324ca3a89f3d7c4
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-14 21:01:49 -0300

    feat: create Avatar component and story docs

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import React, { FC } from 'react';
+import { DefaultAvatarIcon } from './icons/DefaultAvatarIcon';
+import Image from 'next/image';
+import { useTheme } from '@/contexts/ThemeContext';
+
+type AvatarProps = {
+    src?: string;
+};
+export const Avatar: FC<AvatarProps> = ({ src }) => {
+    const { theme } = useTheme();
+    const size = 24;
+
+    return (
+        <div className="p-1">
+            {src ? (
+                <Image
+                    alt="Avatar image"
+                    src={src}
+                    width={size}
+                    height={size}
+                />
+            ) : (
+                <DefaultAvatarIcon
+                    size={size}
+                    fillColor={theme === 'dark' ? 'white' : 'black'}
+                />
+            )}
+        </div>
+    );
+};
---
+++ src/stories/pages/components/Avatar.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Avatar } from '@/components/Avatar';
+
+const meta = {
+    title: 'Components/Avatar',
+    component: Avatar,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof Avatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {},
+};
+
+export const Example: Story = {
+    args: {
+        src: 'https://raw.githubusercontent.com/marcus-castanho/spotify_playlist_guard/main/public/images/guardBot-1db954-circle.png',
+    },
+};
