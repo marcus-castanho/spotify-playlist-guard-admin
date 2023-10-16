435cd8874a97dc2cb6aad575a2cd9747aa61df0e
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-15 22:42:10 -0300

    feat: add 'stretch' prop to ButtonSeconday

--- src/components/ButtonSecondary.tsx
+++ src/components/ButtonSecondary.tsx
@@ -4,20 +4,28 @@ type ButtonSecondaryProps = {
     content: string;
     onClick?: () => void;
     type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
+    stretch?: boolean;
 };
 
 export const ButtonSecondary: FC<ButtonSecondaryProps> = ({
     content,
     onClick = () => {},
     type = 'button',
+    stretch = false,
 }) => {
     return (
-        <button
-            type={type}
-            onClick={onClick}
-            className="rounded-[500px] border-[1px] border-gray-100 bg-white px-8 py-3 font-bold text-gray-100 hover:scale-105 hover:text-black dark:bg-black dark:hover:text-white"
-        >
-            {content}
-        </button>
+        <div>
+            <button
+                type={type}
+                onClick={onClick}
+                className={
+                    stretch
+                        ? 'w-full rounded-[500px] border-[1px] border-gray-100 bg-white px-8 py-3 font-bold text-gray-100 hover:scale-105 hover:text-black dark:bg-black dark:hover:text-white'
+                        : 'rounded-[500px] border-[1px] border-gray-100 bg-white px-8 py-3 font-bold text-gray-100 hover:scale-105 hover:text-black dark:bg-black dark:hover:text-white'
+                }
+            >
+                {content}
+            </button>
+        </div>
     );
 };
