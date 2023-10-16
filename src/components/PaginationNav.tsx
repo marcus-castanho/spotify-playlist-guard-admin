f2e1201780a41997bef06284430fefff4f01053e
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-16 20:23:53 -0300

    feat: add styling to PaginationNav and create story docs

--- src/components/PaginationNav.tsx
+++ src/components/PaginationNav.tsx
@@ -1,5 +1,62 @@
 import { usePagination } from '@/hooks/usePagination';
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
+import { ChevronLeftIcon } from './icons/ChevronLeftIcon';
+import { useTheme } from '@/contexts/ThemeContext';
+import { ChevronRightIcon } from './icons/ChevronRightIcon';
+import { match } from 'ts-pattern';
+
+const GapBox = () => {
+    return <div className="p-2">...</div>;
+};
+
+type PageBoxProps = {
+    page: number;
+    isSelected: boolean;
+};
+const PageBox: FC<PageBoxProps> = ({ page }) => {
+    return (
+        <div className="flex h-6 min-w-[2rem] items-center justify-center">
+            {page}
+        </div>
+    );
+};
+
+type NavButtonProps = {
+    children: ReactNode;
+    onClick: () => void;
+    disabled?: boolean;
+    isSelected?: boolean;
+};
+const NavButton: FC<NavButtonProps> = ({
+    children,
+    onClick,
+    disabled = false,
+    isSelected = false,
+}) => {
+    return (
+        <button
+            onClick={() => onClick()}
+            className={match({ isSelected, disabled })
+                .with(
+                    { isSelected: true },
+                    () =>
+                        'flex items-center justify-between rounded-md bg-primary-verdant p-1 text-black',
+                )
+                .with(
+                    { disabled: true },
+                    () =>
+                        'flex items-center justify-between rounded-md border-[1px] border-transparent p-1 opacity-50',
+                )
+                .otherwise(
+                    () =>
+                        'flex items-center justify-between rounded-md border-[1px] border-transparent p-1 hover:border-gray-100',
+                )}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+};
 
 type PaginationNavProps = {
     page: number;
@@ -14,33 +71,49 @@ export const PaginationNav: FC<PaginationNavProps> = ({
     changePage,
     pagesIndexes,
 }) => {
+    const { theme } = useTheme();
+    const lastPage = pagesIndexes[pagesIndexes.length - 1];
+
     return (
-        <>
-            <button onClick={() => changePage('previous')}>
-                previous page
-            </button>
-            {pagesIndexes.map((pageIndex) => {
-                if (pageIndex === null) return '...';
-                return (
-                    <button
-                        key={pageIndex}
-                        onClick={() => changePage(pageIndex)}
-                        style={{
-                            border: page === pageIndex ? '1px red solid' : '',
-                        }}
-                    >
-                        {pageIndex}
-                    </button>
-                );
-            })}
-            <button
-                onClick={() => {
-                    if (page === pagesIndexes[pagesIndexes.length - 1]) return;
-                    return changePage('next');
-                }}
+        <div className="flex gap-2 p-4">
+            <NavButton
+                onClick={() => changePage('previous')}
+                disabled={page === 1}
             >
-                next page
-            </button>
-        </>
+                <ChevronLeftIcon
+                    size={16}
+                    fillColor={theme === 'dark' ? 'white' : 'black'}
+                />
+                <div className="pr-2">Previous</div>
+            </NavButton>
+            <div className="flex gap-2 max-sm:hidden">
+                {pagesIndexes.map((pageIndex) =>
+                    pageIndex === null ? (
+                        <GapBox key={pageIndex} />
+                    ) : (
+                        <NavButton
+                            key={pageIndex}
+                            onClick={() => changePage(pageIndex)}
+                            isSelected={page === pageIndex}
+                        >
+                            <PageBox
+                                page={pageIndex}
+                                isSelected={page === pageIndex}
+                            />
+                        </NavButton>
+                    ),
+                )}
+            </div>
+            <NavButton
+                onClick={() => changePage('next')}
+                disabled={page === lastPage}
+            >
+                <div className="pl-2">Next</div>
+                <ChevronRightIcon
+                    size={16}
+                    fillColor={theme === 'dark' ? 'white' : 'black'}
+                />
+            </NavButton>
+        </div>
     );
 };
