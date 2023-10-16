e94b0d34789898549a77f7c730596ee07786cfb9
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-15 21:40:13 -0300

    feat: remove content from Index

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import Link from 'next/link';
-import { Header } from '@/components/Header';
 
 export const Index = () => {
-    return (
-        <>
-            <Header />
-            <Link href="/signin">Sign in</Link>
-        </>
-    );
+    return <></>;
 };
