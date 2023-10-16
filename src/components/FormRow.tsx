37f208e3521327714e35194db8d34ad53e96034f
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-15 23:49:37 -0300

    feat: exchange margin to padding in FormRow

--- src/components/FormRow.tsx
+++ src/components/FormRow.tsx
@@ -8,11 +8,11 @@ type FormRowProps = {
 
 export const FormRow: FC<FormRowProps> = ({ columns, children }) => {
     const styleClass = match(columns)
-        .with(1, () => 'mb-3.5 grid grid-cols-1 gap-3.5')
-        .with(2, () => 'mb-3.5 grid grid-cols-2 gap-3.5')
-        .with(3, () => 'mb-3.5 grid grid-cols-3 gap-3.5')
-        .with(4, () => 'mb-3.5 grid grid-cols-4 gap-3.5')
-        .otherwise(() => 'mb-3.5 grid grid-cols-1 gap-3.5');
+        .with(1, () => 'pb-3.5 grid grid-cols-1 gap-3.5')
+        .with(2, () => 'pb-3.5 grid grid-cols-2 gap-3.5')
+        .with(3, () => 'pb-3.5 grid grid-cols-3 gap-3.5')
+        .with(4, () => 'pb-3.5 grid grid-cols-4 gap-3.5')
+        .otherwise(() => 'pb-3.5 grid grid-cols-1 gap-3.5');
 
     return <div className={styleClass}>{children}</div>;
 };
