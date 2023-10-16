0daccca2e44881d3c212b8573156959e6839a2a4
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-15 22:46:45 -0300

    feat: create ProfileVisualizer component

---
+++ src/views/Profile/components/ProfileVisualizer.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react';
+import { ButtonPrimary } from '@/components/ButtonPrimary';
+import { FormRow } from '@/components/FormRow';
+import { TextInputField } from '@/components/TextInputField';
+
+type ProfileVisualizerProps = {
+    defaultForm: { name: string; email: string };
+    onEdit: () => void;
+};
+
+export const ProfileVisualizer: FC<ProfileVisualizerProps> = ({
+    defaultForm,
+    onEdit,
+}) => {
+    return (
+        <div>
+            <FormRow columns={1}>
+                <TextInputField
+                    id="name"
+                    label="Name"
+                    defaultValue={defaultForm.name}
+                    onChange={() => {}}
+                    disabled
+                />
+            </FormRow>
+            <FormRow columns={1}>
+                <TextInputField
+                    id="email"
+                    label="E-mail"
+                    defaultValue={defaultForm.email}
+                    onChange={() => {}}
+                    disabled
+                />
+            </FormRow>
+            <div className="flex justify-center p-4">
+                <ButtonPrimary
+                    content="Edit"
+                    type="submit"
+                    onClick={() => onEdit()}
+                />
+            </div>
+        </div>
+    );
+};
