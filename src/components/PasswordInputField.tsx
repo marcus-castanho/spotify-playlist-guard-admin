59fab88b0158c78c8d12818a5cde84414e2cf29a
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-17 21:07:52 -0300

    fix: align content on bottom in input fields components
    -PasswrdInputField;
    -TextInputField;

--- src/components/PasswordInputField.tsx
+++ src/components/PasswordInputField.tsx
@@ -26,7 +26,7 @@ export const PasswordInputField: FC<PasswordInputFieldProps> = ({
     const { theme } = useTheme();
 
     return (
-        <div className="rounded-lg bg-gray-50 p-4 dark:bg-black">
+        <div className="flex flex-col justify-end rounded-lg bg-gray-50 p-4 dark:bg-black">
             <label htmlFor={id} className="block pb-2 font-bold">
                 {label}
                 {required && <span className="pl-1 text-secondary-red">*</span>}
--- src/components/TextInputField.tsx
+++ src/components/TextInputField.tsx
@@ -25,7 +25,7 @@ export const TextInputField: FC<TextInputFieldProps> = ({
     disabled = false,
 }) => {
     return (
-        <div className="rounded-lg bg-gray-50 p-4 dark:bg-black">
+        <div className="flex flex-col justify-end rounded-lg bg-gray-50 p-4 dark:bg-black">
             <label htmlFor={id} className="block pb-2 font-bold">
                 {label}
                 {required && <span className="pl-1 text-secondary-red">*</span>}
