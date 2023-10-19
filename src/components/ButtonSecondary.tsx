69ee88a2b00db6325a2d32190723b4212d2f1d1e
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-18 23:10:07 -0300

    feat: add children and round props to ButtonSecondary

--- src/components/ButtonSecondary.tsx
+++ src/components/ButtonSecondary.tsx
@@ -1,28 +1,36 @@
-import React, { FC, ButtonHTMLAttributes } from 'react';
+import React, { FC, ButtonHTMLAttributes, ReactNode } from 'react';
 
 type ButtonSecondaryProps = {
+    children: ReactNode;
     content: string;
     onClick?: () => void;
     type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
     scale?: boolean;
+    round?: boolean;
 };
 
 export const ButtonSecondary: FC<ButtonSecondaryProps> = ({
+    children,
     content,
     onClick = () => {},
     type = 'button',
     scale = true,
+    round = false,
 }) => {
     return (
-        <button type={type} onClick={onClick}>
+        <button
+            type={type}
+            onClick={onClick}
+            className={scale ? 'hover:scale-105' : ''}
+        >
             <div
                 className={
-                    scale
-                        ? 'rounded-[500px] border-[1px] border-gray-100 bg-white px-8 py-3 font-bold text-gray-100 hover:scale-105 hover:text-black dark:bg-black dark:hover:text-white'
+                    round
+                        ? 'rounded-[500px] border-[1px] border-gray-100 bg-white p-3 font-bold text-gray-100  hover:text-black dark:bg-black dark:hover:text-white'
                         : 'rounded-[500px] border-[1px] border-gray-100 bg-white px-8 py-3 font-bold text-gray-100 hover:text-black dark:bg-black dark:hover:text-white'
                 }
             >
-                {content}
+                {children || content}
             </div>
         </button>
     );
