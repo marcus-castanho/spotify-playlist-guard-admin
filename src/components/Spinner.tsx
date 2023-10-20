cf0a16cf05e04fbaf989a17275e3113d88b74ba7
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-19 22:23:42 -0300

    feat: create Spinner and story docs

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Spinner = () => {
+    return (
+        <span className="inline-block h-12 w-12 animate-spin rounded-[50%] border-4 border-primary-verdant border-b-transparent" />
+    );
+};
