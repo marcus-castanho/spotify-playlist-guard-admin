4a389bd9d09c0660e12013d44eb5341e1d8d5e9d
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-10-24 21:27:21 -0300

    feat: create ContextsProviders mock

---
+++ src/tests/mocks/ContextsProviders-mock.tsx
@@ -0,0 +1,31 @@
+import React, { ComponentProps, ReactNode } from 'react';
+import { AppRouterContextProviderMock } from './AppRouterContextProviderMock';
+import { AppContextProvider } from '@/contexts';
+import './window-mock';
+
+const initialTheme: ComponentProps<typeof AppContextProvider>['initialTheme'] =
+    'light';
+
+const defaultUser: ComponentProps<typeof AppContextProvider>['defaultUser'] = {
+    id: '00000000-0000-0000-0000-000000000000',
+    name: 'Test user',
+    email: 'test@test.com',
+    roles: ['admin'],
+    createdAt: '2023-08-13T02:31:45.610Z',
+    updatedAt: '2023-10-11T01:18:06.082Z',
+};
+
+export function ContextsProvidersMock({ children }: { children: ReactNode }) {
+    const push = jest.fn();
+
+    return (
+        <AppRouterContextProviderMock router={{ push }}>
+            <AppContextProvider
+                initialTheme={initialTheme}
+                defaultUser={defaultUser}
+            >
+                {children}
+            </AppContextProvider>
+        </AppRouterContextProviderMock>
+    );
+}
