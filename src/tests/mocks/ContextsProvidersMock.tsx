0b828cbf202fa4ddadf6d28fc722ae79d997195c
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-02 16:46:21 -0300

    feat: accept values as params in ContextProvidersMock

--- src/tests/mocks/ContextsProvidersMock.tsx
+++ src/tests/mocks/ContextsProvidersMock.tsx
@@ -3,10 +3,13 @@ import { AppRouterContextProviderMock } from './AppRouterContextProviderMock';
 import { AppContextProvider } from '@/contexts';
 import './windoMock';
 
-const initialTheme: ComponentProps<typeof AppContextProvider>['initialTheme'] =
-    'light';
+export const INITIAL_THEME: ComponentProps<
+    typeof AppContextProvider
+>['initialTheme'] = 'light';
 
-const defaultUser: ComponentProps<typeof AppContextProvider>['defaultUser'] = {
+export const DEFAULT_USER: NonNullable<
+    ComponentProps<typeof AppContextProvider>['defaultUser']
+> = {
     id: '00000000-0000-0000-0000-000000000000',
     name: 'Test user',
     email: 'test@test.com',
@@ -15,7 +18,16 @@ const defaultUser: ComponentProps<typeof AppContextProvider>['defaultUser'] = {
     updatedAt: '2023-10-11T01:18:06.082Z',
 };
 
-export function ContextsProvidersMock({ children }: { children: ReactNode }) {
+type ContextsProvidersMockProps = {
+    children: ReactNode;
+    initialTheme?: ComponentProps<typeof AppContextProvider>['initialTheme'];
+    defaultUser?: ComponentProps<typeof AppContextProvider>['defaultUser'];
+};
+export function ContextsProvidersMock({
+    children,
+    initialTheme = INITIAL_THEME,
+    defaultUser = DEFAULT_USER,
+}: ContextsProvidersMockProps) {
     const push = jest.fn();
 
     return (
