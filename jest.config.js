4c2582b0f22e299df3e41537336a0a2351e88ef7
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-03 19:07:40 -0300

    chore: configure jest.setup as typescript

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({ dir: './' });
 
 const config = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testEnvironment: 'jest-environment-jsdom',
 };
 
