0eb06df43edc5a6811e3698b0e745992966a44af
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-08 21:07:04 -0300

    chore: move jest.setup.ts config to .jest/ folder

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({ dir: './' });
 
 const config = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.ts'],
     testEnvironment: 'jest-environment-jsdom',
 };
 
