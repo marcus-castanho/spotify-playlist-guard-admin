4fa3db593dfc9ed98a127c32b329584efb440d36
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-20 13:02:05 -0300

    feat: add size test to GuardBotLogo

--- src/tests/GuardBotLogo.spec.tsx
+++ src/tests/GuardBotLogo.spec.tsx
@@ -4,12 +4,30 @@ import { GuardBotLogo } from '@/components/GuardBotLogo';
 
 const LABEL = 'guard-bot-logo';
 
+const LOGO_ICON_TITLE = 'guard-icon';
+
 describe('GuardBotLogo', () => {
     it('should render', () => {
-        const { getByLabelText } = render(<GuardBotLogo />);
+        const DEFAULT_SIZE = 40;
+        const { getByLabelText, getByTitle } = render(<GuardBotLogo />);
 
         const container = getByLabelText(LABEL);
+        const svgIconTitle = getByTitle(LOGO_ICON_TITLE);
+        const svg = svgIconTitle.parentElement;
 
         expect(container).toBeDefined();
+        expect(svg?.getAttribute('width')).toBe(`${DEFAULT_SIZE}`);
+        expect(svg?.getAttribute('height')).toBe(`${DEFAULT_SIZE}`);
+    });
+
+    it('should render with given size', () => {
+        const SIZE = 60;
+        const { getByTitle } = render(<GuardBotLogo size={SIZE} />);
+
+        const svgIconTitle = getByTitle(LOGO_ICON_TITLE);
+        const svg = svgIconTitle.parentElement;
+
+        expect(svg?.getAttribute('width')).toBe(`${SIZE}`);
+        expect(svg?.getAttribute('height')).toBe(`${SIZE}`);
     });
 });
