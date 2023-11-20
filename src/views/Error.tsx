bb789e4e96f5461774025cbb6d187d78b25e53ea
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-20 17:37:33 -0300

    feat: define styles for Error page

--- src/views/Error.tsx
+++ src/views/Error.tsx
@@ -2,6 +2,12 @@
 
 import React, { useEffect, FC } from 'react';
 import { handleUncaughtClientError } from '@/errors/clientErrorHandlers';
+import { PageContainer } from '@/components/PageContainer';
+import { Main } from '@/components/Main';
+import { ServerWarningIcon } from '@/components/icons/ServerWarningIcon';
+import { Anchor } from '@/components/Anchor';
+import { ButtonSecondary } from '@/components/ButtonSecondary';
+import { useTheme } from '@/contexts/ThemeContext';
 
 export type ErrorProps = {
     error: Error;
@@ -9,15 +15,42 @@ export type ErrorProps = {
 };
 
 export const Error: FC<ErrorProps> = ({ error, reset }) => {
+    const { theme } = useTheme();
+
     useEffect(() => {
         if (!error) return;
         handleUncaughtClientError(error);
     }, [error]);
 
     return (
-        <>
-            <h2>Some unexpcted error happened!</h2>
-            <button onClick={() => reset()}>Try again</button>
-        </>
+        <PageContainer>
+            <Main>
+                <div className="flex w-full items-center justify-center">
+                    <div className="flex flex-col items-center">
+                        <ServerWarningIcon
+                            size={60}
+                            fillColor={theme === 'dark' ? 'white' : 'black'}
+                        />
+                        <h1 className="flex justify-center px-6 py-10 text-center text-3xl font-bold sm:text-left">
+                            Sorry, something went wrong
+                        </h1>
+                        <div className="pb-10">
+                            <ButtonSecondary
+                                scale={false}
+                                onClick={() => reset()}
+                            >
+                                Try again
+                            </ButtonSecondary>
+                        </div>
+                        <Anchor
+                            text="Go Home"
+                            href="/"
+                            target="_self"
+                            label="home-link"
+                        />
+                    </div>
+                </div>
+            </Main>
+        </PageContainer>
     );
 };
