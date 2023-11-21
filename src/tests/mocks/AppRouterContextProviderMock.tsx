d483b5a9ec5aeee0484f406bb7db58a2e88d77c8
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-21 17:19:15 -0300

    fix: import AppRouterContext and AppRouterInstance from new path

--- src/tests/mocks/AppRouterContextProviderMock.tsx
+++ src/tests/mocks/AppRouterContextProviderMock.tsx
@@ -1,7 +1,7 @@
 import {
     AppRouterContext,
     AppRouterInstance,
-} from 'next/dist/shared/lib/app-router-context';
+} from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import React from 'react';
 
 type AppRouterContextProviderMockProps = {
