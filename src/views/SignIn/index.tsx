edb5d86d8f451c42bdf6f036a9d8bccc719b2d9d
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-27 11:54:09 -0300

    feat: handle sesionEnd flow in /signin page

--- src/views/SignIn/index.tsx
+++ src/views/SignIn/index.tsx
@@ -4,20 +4,23 @@ import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { PageContainer } from '@/components/PageContainer';
 import { Main } from '@/components/Main';
+import { SessionEndWrapper } from './components/SessionEndWrapper';
 
 export const SignIn = () => {
     return (
-        <PageContainer>
-            <Header />
-            <Main>
-                <div className="flex w-full items-center justify-center">
-                    <div>
-                        <h1 className="px-4 text-4xl sm:py-4">Sign in</h1>
-                        <SignInForm />
+        <SessionEndWrapper>
+            <PageContainer>
+                <Header />
+                <Main>
+                    <div className="flex w-full items-center justify-center">
+                        <div>
+                            <h1 className="px-4 text-4xl sm:py-4">Sign in</h1>
+                            <SignInForm />
+                        </div>
                     </div>
-                </div>
-            </Main>
-            <Footer />
-        </PageContainer>
+                </Main>
+                <Footer />
+            </PageContainer>
+        </SessionEndWrapper>
     );
 };
