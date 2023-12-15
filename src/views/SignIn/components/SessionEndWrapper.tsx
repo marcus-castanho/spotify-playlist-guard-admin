e89af2b5180df46bcfa1966cd01ac9485378fac4
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-12-14 23:26:27 -0300

    fix: remove queryParams on sessionEnd flow

--- src/views/SignIn/components/SessionEndWrapper.tsx
+++ src/views/SignIn/components/SessionEndWrapper.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { FC, ReactNode, useEffect } from 'react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { useToast } from '@/contexts/ToastContext';
 import { useCookies } from '@/contexts/CookiesContext';
 import { TOKEN_COOKIE_KEY } from '@/contexts/AuthContext';
@@ -12,7 +12,6 @@ type SessionEndWrapperProps = {
 
 export const SessionEndWrapper: FC<SessionEndWrapperProps> = ({ children }) => {
     const { deleteCookie } = useCookies();
-    const router = useRouter();
     const pathname = usePathname();
     const searchParams = useSearchParams();
     const sessionEnd = searchParams?.get('sessionEnd') || null;
@@ -25,11 +24,12 @@ export const SessionEndWrapper: FC<SessionEndWrapperProps> = ({ children }) => {
                 'Youre session was expired. Please sign in to continue',
                 'error',
             );
-            if (pathname) router.push(pathname);
+
+            window.history.pushState({}, document.title, '/signin');
         }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [router, sessionEnd, pathname]);
+    }, [sessionEnd, pathname]);
 
     return <>{children}</>;
 };
