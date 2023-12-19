4a5475604aa7d89b8751a4fdd9983319acc10112
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-12-18 23:23:12 -0300

    chore: change story export from ButtonSecondary

--- src/stories/pages/components/ButtonSecondary.stories.tsx
+++ src/stories/pages/components/ButtonSecondary.stories.tsx
@@ -1,4 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import type { Meta } from '@storybook/react';
 import { ButtonSecondary } from '@/components/ButtonSecondary';
 
 const meta = {
@@ -11,8 +12,5 @@ const meta = {
 } satisfies Meta<typeof ButtonSecondary>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Button: Story = {
-    args: { children: 'Button' },
-};
+export const Button = () => <ButtonSecondary>Button</ButtonSecondary>;
