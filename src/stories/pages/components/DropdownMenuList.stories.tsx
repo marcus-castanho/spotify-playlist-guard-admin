01cb91767604d44055d97e9343880ed053cc29fa
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-12-18 23:24:31 -0300

    chore: change story export in DropdownMenuList

--- src/stories/pages/components/DropdownMenuList.stories.tsx
+++ src/stories/pages/components/DropdownMenuList.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import { DropdownMenuList } from '@/components/DropdownMenuList';
 import { Avatar } from '@/components/Avatar';
 
@@ -10,26 +10,27 @@ const meta = {
 } satisfies Meta<typeof DropdownMenuList>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-    args: {
-        itemsGroups: [
+export const Default = () => (
+    <DropdownMenuList
+        itemsGroups={[
             ['Group1 - Item1', 'Group1 - Item2'],
             ['Group2 - Item1', 'Group2 - Item2', 'Group2 - Item3'],
             ['Group3 - Item1'],
-        ],
-    },
-};
+        ]}
+        onClose={() => {}}
+    />
+);
 
-export const WithHeader: Story = {
-    args: {
-        header: (
+export const WithHeader = () => (
+    <DropdownMenuList
+        header={
             <div className="flex items-center gap-2">
                 <Avatar size={30} />
                 {'Test user'}
             </div>
-        ),
-        itemsGroups: [['Profile', 'Configuration'], ['Sign out']],
-    },
-};
+        }
+        itemsGroups={[['Profile', 'Configuration'], ['Sign out']]}
+        onClose={() => {}}
+    />
+);
